9b06354a139b5db0ed02cc55e19edb58810d08eb
victorgois <victorgois18@gmail.com>
2019-05-16 20:13:36 -0300

    criando as funções de divisão da base de dados

--- script.py
+++ script.py
@@ -16,5 +16,35 @@ def convert_col():
 
     return (col_a)
 
-res = convert_col()
-print(res)
+    #res = convert_col()
+    #print(res)
+
+def divide_chunks(l, n):
+
+    for i in range(0, len(l), n):
+        
+        yield l[i:i + n]
+
+def loop_for_chunks(divided_list):
+
+    length = len(divided_list)
+    
+    for i in range(length):
+        
+        return i
+
+divided_list = list(divide_chunks(convert_col(), 10))
+
+
+def main (divided_list):
+
+    for each in divided_list:
+        
+        return (each)
+    
+length = len(divided_list)
+
+for i in divided_list:
+    result = i
+    print (result)
+    
--- tweetbotornot.r
+++ tweetbotornot.r
@@ -4,25 +4,13 @@ library(rtweet)
 library(reticulate)
 
 ## select users
-users <- c(py_run_file("script.py"))
-#vector <- unlist(users)
-## get botornot estimates
-data <- botornot(users)
+source_python("script.py")
+divided_list = list(divide_chunks(convert_col(), 10))
 
-
-## arrange by prob ests
-data[order(data$prob_bot), ]
-#> # A tibble: 11 x 3
-#>    screen_name     user_id            prob_bot
-#>    <chr>           <chr>                 <dbl>
-#>  1 hadleywickham   69133574            0.00754
-#>  2 realDonaldTrump 25073877            0.00995
-#>  3 kearneymw       2973406683          0.0607
-#>  4 ma_salmon       2865404679          0.150
-#>  5 juliasilge      13074042            0.162
-#>  6 dataandme       3230388598          0.227
-#>  7 hrbrmstr        5685812             0.320
-#>  8 netflix_bot     1203840834          0.978
-#>  9 tidyversetweets 935569091678691328  0.997
-#> 10 mothgenerator   3277928935          0.998
-#> 11 American__Voter 829792389925597184  1.000
+for (each in divided_list:{
+  users <- each
+  unlist(users)
+  data <- botornot(users)
+  print(users)
+  data[order(data$prob_bot), ]
+ })
